1689aaa54b8e247d9ae4d137e00668fd2281010a
Thibault <klein.thib@gmail.com>
2022-06-29 01:03:25 +0200

    working graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,120 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'apificators';
+
+  multiAxisOptions: any;
+
+  lineStylesData: any;
+  
+  basicOptions: any;
+
+  constructor() {}
+
+  ngOnInit() {
+
+    this.lineStylesData = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'First Dataset',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: false,
+                tension: .4,
+                borderColor: '#42A5F5'
+            },
+            {
+                label: 'Second Dataset',
+                data: [28, 48, 40, 19, 86, 27, 90],
+                fill: false,
+                borderDash: [5, 5],
+                tension: .4,
+                borderColor: '#66BB6A'
+            },
+            {
+                label: 'Third Dataset',
+                data: [12, 51, 62, 33, 21, 62, 45],
+                fill: true,
+                borderColor: '#FFA726',
+                tension: .4,
+                backgroundColor: 'rgba(255,167,38,0.2)'
+            }
+        ]
+    };
+    this.applyDarkTheme()
+}
+
+applyDarkTheme() {
+  this.basicOptions = {
+      plugins: {
+          legend: {
+              labels: {
+                  color: '#ebedef'
+              }
+          }
+      },
+      scales: {
+          x: {
+              ticks: {
+                  color: '#ebedef'
+              },
+              grid: {
+                  color: 'rgba(255,255,255,0.2)'
+              }
+          },
+          y: {
+              ticks: {
+                  color: '#ebedef'
+              },
+              grid: {
+                  color: 'rgba(255,255,255,0.2)'
+              }
+          }
+      }
+  };
+
+  this.multiAxisOptions = {
+      stacked: false,
+      plugins: {
+          legend: {
+              labels: {
+                  color: '#ebedef'
+              }
+          }
+      },
+      scales: {
+          x: {
+              ticks: {
+                  color: '#ebedef'
+              },
+              grid: {
+                  color: 'rgba(255,255,255,0.2)'
+              }
+          },
+          y: {
+              type: 'linear',
+              display: true,
+              position: 'left',
+              ticks: {
+                  color: '#ebedef'
+              },
+              grid: {
+                  color: 'rgba(255,255,255,0.2)'
+              }
+          },
+          y1: {
+              type: 'linear',
+              display: true,
+              position: 'right',
+              ticks: {
+                  color: '#ebedef'
+              },
+              grid: {
+                  drawOnChartArea: false,
+                  color: 'rgba(255,255,255,0.2)'
+              }
+          }
+      }
+  };
+}
+
 }

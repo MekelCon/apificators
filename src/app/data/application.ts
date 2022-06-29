d384bfeefc090a7d2f4bb56c305013d9cff17f14
Thibault <klein.thib@gmail.com>
2022-06-29 21:07:08 +0200

    status history

--- src/app/data/application.ts
+++ src/app/data/application.ts
@@ -2,10 +2,12 @@ export class Application {
   public name: string = "";
   public image: string = "";
   public category: string = "";
-  public status: string = "";
+  public statusToday: string = "";
+  public statusJ7: string = "";
+  public statusJ15: string = "";
 
   static myApplications: Application[] = [
-    { 'name': 'EBOO', 'image': 'eboo-logo.png', 'category': 'bancaire', 'status': 'ATTACKED' },
-    { 'name': 'R-Net', 'image': 'raffeisen-logo.jpg', 'category': 'bancaire', 'status': 'NOTHING' }
+    { 'name': 'EBOO', 'image': 'eboo-logo.png', 'category': 'bancaire', 'statusToday': 'ATTACKED', 'statusJ7': 'NOTHING', 'statusJ15': 'NOTHING' },
+    { 'name': 'R-Net', 'image': 'raffeisen-logo.jpg', 'category': 'bancaire', 'statusToday': 'NOTHING', 'statusJ7': 'NOTHING', 'statusJ15': 'NOTHING' }
   ];
 }

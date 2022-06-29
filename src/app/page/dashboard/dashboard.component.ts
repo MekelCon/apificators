8be90879fe5abc590cfa5bc3e7000923ec41fdde
Thibault <klein.thib@gmail.com>
2022-06-29 04:55:52 +0200

    fix reference

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -6,118 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  multiAxisOptions: any;
+  data: any;
 
-  lineStylesData: any;
-
-  basicOptions: any;
+  chartOptions: any;
 
   constructor() { }
 
   ngOnInit() {
-
-    this.lineStylesData = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    this.data = {
+      labels: ['A', 'B', 'C'],
       datasets: [
         {
-          label: 'First Dataset',
-          data: [65, 59, 80, 81, 56, 55, 40],
-          fill: false,
-          tension: .4,
-          borderColor: '#42A5F5'
-        },
-        {
-          label: 'Second Dataset',
-          data: [28, 48, 40, 19, 86, 27, 90],
-          fill: false,
-          borderDash: [5, 5],
-          tension: .4,
-          borderColor: '#66BB6A'
-        },
-        {
-          label: 'Third Dataset',
-          data: [12, 51, 62, 33, 21, 62, 45],
-          fill: true,
-          borderColor: '#FFA726',
-          tension: .4,
-          backgroundColor: 'rgba(255,167,38,0.2)'
+          data: [300, 50, 100],
+          backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ]
         }
       ]
     };
-    this.applyDarkTheme()
+    this.getDarkTheme()
   }
-
-  applyDarkTheme() {
-    this.basicOptions = {
+  
+  getDarkTheme() {
+    return {
       plugins: {
         legend: {
           labels: {
             color: '#ebedef'
           }
         }
-      },
-      scales: {
-        x: {
-          ticks: {
-            color: '#ebedef'
-          },
-          grid: {
-            color: 'rgba(255,255,255,0.2)'
-          }
-        },
-        y: {
-          ticks: {
-            color: '#ebedef'
-          },
-          grid: {
-            color: 'rgba(255,255,255,0.2)'
-          }
-        }
       }
-    };
-
-    this.multiAxisOptions = {
-      stacked: false,
-      plugins: {
-        legend: {
-          labels: {
-            color: '#ebedef'
-          }
-        }
-      },
-      scales: {
-        x: {
-          ticks: {
-            color: '#ebedef'
-          },
-          grid: {
-            color: 'rgba(255,255,255,0.2)'
-          }
-        },
-        y: {
-          type: 'linear',
-          display: true,
-          position: 'left',
-          ticks: {
-            color: '#ebedef'
-          },
-          grid: {
-            color: 'rgba(255,255,255,0.2)'
-          }
-        },
-        y1: {
-          type: 'linear',
-          display: true,
-          position: 'right',
-          ticks: {
-            color: '#ebedef'
-          },
-          grid: {
-            drawOnChartArea: false,
-            color: 'rgba(255,255,255,0.2)'
-          }
-        }
-      }
-    };
+    }
   }
 }
